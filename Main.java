7c4f91c5afda79b3ef84c6a92a7453d87de6a441
karofelek666 <100691829+Bartossczep13@users.noreply.github.com>
2022-03-15 12:15:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+  int i;
+  System.out.println("Liczby parzyste");
+    for(i=1;i<=100;i++)
+      {
+        if(i%2==0)
+        {
+          System.out.println(i);
+        }
+      }
+
+
+
+    
+  }
+}
